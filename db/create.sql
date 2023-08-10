e08cca9359479b123f70a75573b50974987e60df
ledkaa <ledkaa@gmai.com>
2023-08-10 08:34:41 +0100

    update DB

--- db/create.sql
+++ db/create.sql
@@ -14,7 +14,7 @@ CREATE TABLE Drivers (
     driver_id INT PRIMARY KEY,
     user_id INT UNIQUE,
     driver_license_picture_url VARCHAR(255),
-    car_description TEXT,
+    car_description VARCHAR (2555),
     car_model VARCHAR(100),
     car_plate_number VARCHAR(20),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
@@ -28,36 +28,48 @@ CREATE TABLE Passengers (
 );
 
 
-CREATE TABLE Rides (
-    ride_id INT PRIMARY KEY,
+CREATE TABLE Travels (
+    travel_id INT PRIMARY KEY,
     driver_id INT,
-    departure_location VARCHAR(255) NOT NULL,
-    destination_location VARCHAR(255) NOT NULL,
+    start_location VARCHAR(255) NOT NULL,
+    end_location VARCHAR(255) NOT NULL,
     departure_datetime DATETIME NOT NULL,
+    cost_per_person VARCHAR(45) NOT NULL ,
     available_seats INT NOT NULL,
-    description TEXT,
+    description VARCHAR(2222),
     FOREIGN KEY (driver_id) REFERENCES Drivers(driver_id)
 );
 
 
 CREATE TABLE RideRequests (
     request_id INT PRIMARY KEY,
-    ride_id INT,
+    travel_id INT,
     passenger_id INT,
     request_status VARCHAR(20) NOT NULL,
-    FOREIGN KEY (ride_id) REFERENCES Rides(ride_id),
+    FOREIGN KEY (travel_id) REFERENCES Travels(travel_id) ,
     FOREIGN KEY (passenger_id) REFERENCES Passengers(passenger_id)
 );
 
 
 CREATE TABLE Ratings (
-    rating_id INT PRIMARY KEY,
-    rated_user_id INT,
-    rated_by_user_id INT,
-    ride_id INT,
-    rating_value INT NOT NULL,
-    comment TEXT,
-    FOREIGN KEY (rated_user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (rated_by_user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (ride_id) REFERENCES Rides(ride_id)
+                           rating_id INT PRIMARY KEY,
+                           rated_user_id INT,
+                           rated_by_user_id INT,
+                           ride_id INT,
+                           rating_value INT NOT NULL,
+                           comment VARCHAR (2555) NOT NULL ,
+                           FOREIGN KEY (rated_user_id) REFERENCES Users(user_id),
+                           FOREIGN KEY (rated_by_user_id) REFERENCES Users(user_id),
+                           FOREIGN KEY (ride_id) REFERENCES Travels(travel_id)
+);
+
+CREATE TABLE Cars (
+    car_id INT PRIMARY KEY ,
+    driver_id INT NOT NULL ,
+    car_brand VARCHAR (45) NOT NULL ,
+    car_model VARCHAR (45) NOT NULL ,
+    color VARCHAR (45) NOT NULL ,
+    car_year INT NOT NULL ,
+    description VARCHAR (1222) NOT NULL ,
+    FOREIGN KEY (driver_id) REFERENCES Drivers(driver_id)
 );
