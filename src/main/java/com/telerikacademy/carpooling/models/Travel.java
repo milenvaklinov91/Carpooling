2c878b0b14e89f87d08a3408e110fce657e3811b
milenvaklinov <milenvaklinov@gmail.com>
2023-08-10 15:52:27 +0300

    add travel model

--- src/main/java/com/telerikacademy/carpooling/models/Travel.java
+++ src/main/java/com/telerikacademy/carpooling/models/Travel.java
@@ -1,6 +1,82 @@
 package com.telerikacademy.carpooling.models;
 
+import java.time.LocalDateTime;
+
 public class Travel {
 
+    private int travelId;
+    private int driverId;
+    private String startLocation;
+    private String endLocation;
+    private LocalDateTime departureTime;
+    private String costPerPerson;
+    private String availableSeats;
+    private String description;
 
+    public Travel() {
+    }
+
+    public int getTravelId() {
+        return travelId;
+    }
+
+    public void setTravelId(int travelId) {
+        this.travelId = travelId;
+    }
+
+    public int getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(int driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getEndLocation() {
+        return endLocation;
+    }
+
+    public void setEndLocation(String endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getCostPerPerson() {
+        return costPerPerson;
+    }
+
+    public void setCostPerPerson(String costPerPerson) {
+        this.costPerPerson = costPerPerson;
+    }
+
+    public String getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(String availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
