6d41be96f5aa51d9faaaaaf6919c11a607f2f90f
championsocks <kokostanev20@gmail.com>
2023-08-13 00:07:54 +0300

    CarDto update

--- src/main/java/com/telerikacademy/carpooling/models/dtos/CarDto.java
+++ src/main/java/com/telerikacademy/carpooling/models/dtos/CarDto.java
@@ -15,7 +15,7 @@ public class CarDto {
     @NotBlank
     private String brand;
     @NotNull(message = "Model can't be empty")
-    @Size(min = 2, max = 20, message = "Model should be between 2 and 20 symbols")
+    @Size(min = 1, max = 20, message = "Model should be between 1 and 20 symbols")
     @NotBlank
     private String model;
     @Size(min = 1, max = 7, message = "Car capacity should be between 1 and 7 seats")
@@ -25,8 +25,12 @@ public class CarDto {
     @Size(min = 2, max = 20, message = "The color of the car should be between 2 and 20 symbols")
     @NotBlank
     private String colour;
-
-    private String year;
+    @NotNull(message = "Car year can't be empty")
+    @Size(min = 1900, max = 2023, message = "The car year should be between 1900 and 2023.")
+    @NotBlank
+    private Integer year;
+    @Size(min = 5, max = 255, message = "The car description should be between 5 and 255 symbols")
+    @NotBlank
     private String description;
     private boolean isExtraStorage;
     private boolean isSmoke;
