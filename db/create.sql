78914b2560b36ade692c72b6db3792b9a8c3c819
milenvaklinov <milenvaklinov@gmail.com>
2023-08-12 19:23:50 +0300

    update

--- db/create.sql
+++ db/create.sql
@@ -1,75 +1,105 @@
-CREATE TABLE Users
+create table users
 (
-    user_id           int auto_increment
+    user_id int auto_increment
         primary key,
-    username          VARCHAR(255) NOT NULL,
-    password          VARCHAR(255) NOT NULL,
-    email             VARCHAR(255) NOT NULL,
-    first_name        VARCHAR(45)  NOT NULL,
-    last_name         VARCHAR(45)  NOT NULL,
-    profile_picture   VARCHAR(255),
-    registration_date DATETIME,
-    phone_number      int(10)      NOT NULL,
-    is_driver         tinyint(1)   NOT NULL,
-    is_blocked        tinyint(1)   NOT NULL
+    username varchar(255) not null,
+    password varchar(255) not null,
+    email varchar(255) not null,
+    first_name varchar(45) not null,
+    last_name varchar(45) not null,
+    profile_picture varchar(255) null,
+    registration_date datetime null,
+    phone_number varchar(10) not null,
+    is_driver tinyint(1) not null,
+    is_blocked tinyint(1) not null,
+    is_admin tinyint(1) not null
 );
 
-
-CREATE TABLE Travels
+create table drivers
 (
-    travel_id          INT AUTO_INCREMENT PRIMARY KEY,
-    driver_id          INT,
-    start_location     VARCHAR(255) NOT NULL,
-    end_location       VARCHAR(255) NOT NULL,
-    departure_datetime DATETIME     NOT NULL,
-    cost_per_person    VARCHAR(45)  NOT NULL,
-    available_seats    INT          NOT NULL,
-    description        VARCHAR(2222)
-
-);
-
-
-CREATE TABLE RideRequests
-(
-    request_id     INT AUTO_INCREMENT PRIMARY KEY,
-    travel_id      INT,
-    user_id        INT,
-    request_status VARCHAR(20) NOT NULL,
-    FOREIGN KEY (travel_id) REFERENCES Travels (travel_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id)
-);
-
-
-CREATE TABLE Ratings
-(
-    rating_id        INT AUTO_INCREMENT PRIMARY KEY,
-    rated_user_id    INT,
-    rated_by_user_id INT,
-    ride_id          INT,
-    rating_value     INT           NOT NULL,
-    comment          VARCHAR(2555) NOT NULL,
-    FOREIGN KEY (rated_user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (rated_by_user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (ride_id) REFERENCES Travels (travel_id)
-);
-
-create table Cars
-(
-    car_id          int AUTO_INCREMENT not null
+    driver_id int auto_increment
         primary key,
-    user_id         int                not null,
-    car_brand       varchar(45)        not null,
-    car_model       varchar(45)        not null,
-    car_color       varchar(45)        not null,
-    car_year        int                not null,
-    description     varchar(1222)      not null,
-    extra_storage   tinyint(1)         not null,
-    smoke           tinyint(1)         not null,
-    air_conditioner tinyint(1)         not null,
-    pet_available   tinyint(1)         not null,
-    consume_food    tinyint(1)         not null,
-    consume_drink   tinyint(1)         not null,
-    car_capacity    int                not null,
-    constraint cars_users_user_id_pk
-        foreign key (user_id) references Users (user_id)
+    user_id int null,
+    driver_license_picture_url varchar(255) null,
+    constraint user_id
+        unique (user_id),
+    constraint drivers_ibfk_1
+        foreign key (user_id) references users (user_id)
 );
+
+create table cars
+(
+    car_id int auto_increment
+        primary key,
+    driver_id int not null,
+    car_brand varchar(45) not null,
+    car_model varchar(45) not null,
+    color varchar(45) not null,
+    car_year int not null,
+    description varchar(1222) not null,
+    constraint cars_ibfk_1
+        foreign key (driver_id) references drivers (driver_id)
+);
+
+create index driver_id
+    on cars (driver_id);
+
+create table travels
+(
+    travel_id int auto_increment
+        primary key,
+    user_id int null,
+    start_location varchar(255) not null,
+    end_location varchar(255) not null,
+    departure_datetime datetime not null,
+    cost_per_person varchar(45) not null,
+    available_seats int not null,
+    description varchar(2222) null,
+    constraint travels_ibfk_1
+        foreign key (user_id) references users (user_id)
+);
+
+create table ratings
+(
+    rating_id int auto_increment
+        primary key,
+    rated_user_id int null,
+    rated_by_user_id int null,
+    ride_id int null,
+    rating_value int not null,
+    comment varchar(2555) not null,
+    constraint ratings_ibfk_1
+        foreign key (rated_user_id) references users (user_id),
+    constraint ratings_ibfk_2
+        foreign key (rated_by_user_id) references users (user_id),
+    constraint ratings_ibfk_3
+        foreign key (ride_id) references travels (travel_id)
+);
+
+create index rated_by_user_id
+    on ratings (rated_by_user_id);
+
+create index rated_user_id
+    on ratings (rated_user_id);
+
+create index ride_id
+    on ratings (ride_id);
+
+create table riderequests
+(
+    request_id int auto_increment
+        primary key,
+    travel_id int null,
+    user_id int null,
+    request_status varchar(20) not null,
+    constraint riderequests_ibfk_1
+        foreign key (travel_id) references travels (travel_id),
+    constraint riderequests_ibfk_2
+        foreign key (user_id) references users (user_id)
+);
+
+create index travel_id
+    on riderequests (travel_id);
+
+create index user_id
+    on riderequests (user_id);
