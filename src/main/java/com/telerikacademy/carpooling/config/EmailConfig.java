c1173ce83eed52eb414bfe5a7de829d559b3419c
ledkaa <ledkaa@gmai.com>
2023-08-23 16:35:49 +0100

    email config work

--- src/main/java/com/telerikacademy/carpooling/config/EmailConfig.java
+++ src/main/java/com/telerikacademy/carpooling/config/EmailConfig.java
@@ -16,31 +16,13 @@ public class EmailConfig {
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.zoho.eu");
-        mailSender.setPort(465);
+        mailSender.setPort(587);
         mailSender.setUsername("carpoolingteam11@zohomail.eu");
         mailSender.setPassword("pr0B3n!a");
 
         Properties props = mailSender.getJavaMailProperties();
-       /* props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-      //  props.put("mail.smtp.starttls.required", "true");
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.ssl.trust", "smtp.zoho.eu");*/
-
-        props.setProperty("mail.smtp.host", "smtp.zoho.com");
-        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.setProperty("mail.smtp.socketFactory.fallback", "false");
-        props.setProperty("mail.smtp.port", "465");
-        props.setProperty("mail.smtp.socketFactory.port", "465");
-
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.debug", "true");
-        props.put("mail.store.protocol", "pop3");
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.debug.auth", "true");
-        props.setProperty( "mail.pop3.socketFactory.fallback", "false");
 
         return mailSender;
     }
